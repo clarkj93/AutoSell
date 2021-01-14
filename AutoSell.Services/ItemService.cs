08df8e57dc475784d8ffb1845ef50a1bb67d5224
clarkj93 <jon-clark@hotmail.co.uk>
2021-01-14 16:27:50 +0000

    Add files via upload

---
+++ AutoSell.Services/ItemService.cs
@@ -0,0 +1,16 @@
+﻿using AutoSell.Data.Context;
+using AutoSell.Data.Models;
+using AutoSell.Services.Repositories;
+
+namespace AutoSell.Services
+{
+    public class ItemService : ModelService<Item>, IItemRepository
+    {
+        /// <summary>
+        /// The service used to query the Items table
+        /// </summary>
+        /// <param name="autoSellContext"></param>
+        public ItemService(AutoSellContext autoSellContext) : base(autoSellContext, autoSellContext.Items)
+        { }
+    }
+}
---
+++ Repositories/IItemRepository.cs
@@ -0,0 +1,8 @@
+﻿using AutoSell.Data.Models;
+using AutoSell.Services.Repositories.Base;
+
+namespace AutoSell.Services.Repositories
+{
+    public interface IItemRepository : ISearchable<Item>
+    { }
+}
