bffca7a5448902ad04a1044743feab5dd742d94e
clarkj93 <jon-clark@hotmail.co.uk>
2021-01-15 12:47:35 +0000

    Delete Repositories directory

--- Repositories/IItemRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using AutoSell.Data.Models;
-using AutoSell.Services.Repositories.Base;
-
-namespace AutoSell.Services.Repositories
-{
-    public interface IItemRepository : ISearchable<Item>
-    { }
-}
